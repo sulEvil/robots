7986bbd6a400b8fdf1092a57cd27f68c6f017a35
Sultan Galimov <sultan_galimov@bk.ru>
2022-05-04 19:38:43 +0500

    almost end menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,8 +4,42 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 document.addEventListener('click', function(event){
+    const menu = document.querySelector('.menu__container');
     if(event.target.closest('.icon-menu')){
+        document.querySelector('html').classList.toggle('lock');
         document.querySelector('.menu').classList.toggle('menu_active');
+        console.log(getComputedStyle(menu).bottom)
+        if(getComputedStyle(menu).bottom == '0px'){
+            menu.style.bottom = '100%';
+            menu.style.paddingTop = '0';
+            menu.style.paddingBottom = '0';
+        } else {
+            menu.style.bottom = '0';
+            menu.style.paddingTop = '100px';
+            menu.style.paddingBottom = '80px';
+        }
+
+    }
+    if(event.target.closest('.menu__item_inside')){
     }
 
+
 })
+
+document.addEventListener('mousemove', function(event){
+    if(event.target.closest('.menu__link_inside[data-id]')){
+        console.log('Поймал')
+    }
+})
+
+document.addEventListener('keydown',function(event) {
+    const menu = document.querySelector('.menu__container');
+    // ESCAPE key pressed
+    if (event.keyCode == 27) {
+        document.querySelector('html').classList.remove('lock');
+        document.querySelector('.menu').classList.remove('menu_active');
+        menu.style.bottom = '100%';
+        menu.style.paddingTop = '0';
+        menu.style.paddingBottom = '0';
+    }
+});
