bfb225b41810746448320459774dc476c6006e09
Sultan Galimov <sultan_galimov@bk.ru>
2022-05-04 15:49:19 +0500

    end menu icon

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,10 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+document.addEventListener('click', function(event){
+    if(event.target.closest('.icon-menu')){
+        document.querySelector('.menu').classList.toggle('menu_active');
+    }
+
+})
